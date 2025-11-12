c55da3b6ace2722ebfdcf10f9670c6575118f9fc
ezequiasherrera <ezequiasherrera99@gmail.com>
2025-11-12 17:19:48 -0400

    Trying to install Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-	content: ["./index.html"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+  content: [
+    "./index.html",
+    "./**/*.html",
+    "./css/**/*.css",
+    "./**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkorange: "#b34408" // ahora puedes usar bg-darkorange, text-darkorange, etc.
+      }
+    }
+  },
+  plugins: []
+}
