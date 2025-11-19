586b3a46fd8982b998c71ca705a29c4746987930
ezequiasherrera <ezequiasherrera99@gmail.com>
2025-11-19 09:59:18 -0400

    Removing alert bx on copy to clipboard

--- src/static/js/copy.js
+++ src/static/js/copy.js
@@ -2,6 +2,5 @@ function copyToClipboard() {
     const endereco = document.getElementById("endereco").textContent;
 
     navigator.clipboard.writeText(endereco)
-        .then(() => alert("Endereço copiado: " + endereco))
         .catch(err => console.error("Erro ao copiar:", err));
 }
